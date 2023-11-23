b53805092d4eba3dd16cba5ea73297b9451ad06b
abhits <abhiramits004@gmail.com>
2023-11-23 15:23:07 +0530

    Initial commit

--- arrays/min_consecutive_flips.py
+++ arrays/min_consecutive_flips.py
@@ -17,3 +17,41 @@ o/p: from 0 to 0
 
 '''
 
+
+def min_consecutive_flips(items):
+
+     for i in range(1, len(items)):
+          if items[i] != items[i - 1]:
+               if items[i] != items[0]:
+                    print(f"from index {i}")
+               else:
+                    print(f"to {i - 1}")
+
+     if items[len(items) - 1] != items[0]:
+          print(f"to {len(items) - 1}")
+     print("---------------------------------")
+
+
+
+
+
+
+def test_min_consecutive_flips():
+    # Test Case 1
+    items1 = [1, 1, 0, 0, 0, 1]
+    min_consecutive_flips(items1)
+
+    # Test Case 2
+    items2 = [1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1]
+    min_consecutive_flips(items2)
+
+    # Test Case 3
+    items3 = [1, 1, 1]
+    min_consecutive_flips(items3)
+
+    # Test Case 4
+    items4 = [0, 1]
+    min_consecutive_flips(items4)
+
+if __name__ == "__main__":
+    test_min_consecutive_flips()
